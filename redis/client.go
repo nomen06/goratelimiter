e2ea5195290c25787effb389bf294572ed1cec6c
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-06-18 22:24:02 +0530

    client for the rate limiter

---
+++ redis/client.go
@@ -0,0 +1,47 @@
+package redis
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+)
+
+type Client struct {
+	conn   net.Conn
+	reader *bufio.Reader
+}
+
+func (c *Client) Do(args []string) (string, error) {
+	n := len(args)
+	input := "*"
+	input += strconv.Itoa(n)
+	input += "\r"
+	input += "\n"
+	for comp := range args {
+		len2 := len(args[comp])
+		input += "$"
+		input += strconv.Itoa(len2)
+		input += "\r"
+		input += "\n"
+		input += args[comp]
+		input += "\r"
+		input += "\n"
+	}
+	fmt.Fprint(c.conn, input)
+	response, err := c.reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return response, nil
+}
+func Newclient(addr string) (*Client, error) {
+	con, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	connReader := bufio.NewReader(con)
+	client := Client{conn: con, reader: connReader}
+
+	return &client, nil
+}
